842e868ffd11ad3e57ea677402d57fa4b1635139
Ryan Lail <me@ryanlail.com>
2020-11-12 12:05:43 +0000

    generate boudning box

--- SemanticSegmentationToBoundingBox.py
+++ SemanticSegmentationToBoundingBox.py
@@ -1,9 +1,23 @@
 import json
 
-with fh.open(filename) as fh:
-    data = json.load(fh)
-    labels = data["objects"]
+filename = "../CityScapes/gtFine_trainvaltest/gtFine/train/zurich/zurich_000000_000019_gtFine_polygons.json"
 
-    for label in labels:
+with open(filename) as fh:
+    data = json.load(fh)
+    segments = data["objects"]
+
+    for segment in segments:
         # take labels x and y min and max to generate a bbox
-        pass
+        
+        label = segment["label"]
+        vertices = segment["polygon"]
+        
+        x_vals = [coord[0] for coord in vertices]
+        y_vals = [coord[1] for coord in vertices]
+        
+        x1 = min(x_vals)
+        y1 = min(y_vals)
+        w = max(x_vals) - x1
+        h = max(y_vals) - y1
+
+
